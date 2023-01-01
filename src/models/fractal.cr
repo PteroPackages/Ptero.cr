6762d561603d4af271dec336044301eb089886be
devnote-dev <inferno0745@gmail.com>
2023-01-01 03:01:15 +0000

    feat: implement fractal & user models

---
+++ src/models/fractal.cr
@@ -0,0 +1,15 @@
+module Pterodactyl::Models
+  struct FractalItem(M)
+    include JSON::Serializable
+
+    getter object : String
+    getter attributes : M
+  end
+
+  struct FractalList(M)
+    include JSON::Serializable
+
+    getter object : String
+    getter data : Array(FractalItem(M))
+  end
+end
---
+++ src/models/users.cr
@@ -0,0 +1,31 @@
+module Pterodactyl::Models
+  struct User
+    include JSON::Serializable
+
+    getter id : Int32
+    getter external_id : String?
+    getter uuid : String
+    getter username : String
+    getter email : String
+    getter first_name : String
+    getter last_name : String
+    getter language : String
+    @[JSON::Field(key: "root_admin")]
+    getter? admin : Bool
+    @[JSON::Field(key: "2fa")]
+    getter? two_factor : Bool
+    getter created_at : Time
+    getter updated_at : Time?
+  end
+
+  struct SubUser
+    include JSON::Serializable
+
+    getter id : Int32
+    getter user_id : Int32
+    getter server_id : Int32
+    getter permissions : Array(String)
+    getter created_at : Time
+    getter updated_at : Time?
+  end
+end
