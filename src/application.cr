9a726b89f84958262f37ed4fe5a8710a7ffc5efe
devnote-dev <inferno0745@gmail.com>
2023-01-01 03:26:16 +0000

    feat(application): implement query support

--- src/application.cr
+++ src/application.cr
@@ -18,22 +18,34 @@ module Pterodactyl
       )
     end
 
-    # def resolve_query(page : Int32?, per_page : Int32?) : String
-    #   URI::Params.build do |params|
-    #     params.add("page", page) if page
-    #     params.add("per_page", per_page.min(1).max(100)) if per_page
-    #   end
-    # end
+    def resolve_query(page : Int32?, per_page : Int32?, filter : {String, String}?,
+                      includes : Array(String)?, sort : String?) : String
+      URI::Params.build do |params|
+        params.add("page", page.to_s) if page
+        if value = per_page
+          value = 1 if value < 1
+          value = 100 if value > 100
+          params.add("per_page", value.to_s)
+        end
+        params.add("filter[#{filter[0]}]", filter[1]) if filter
+        params.add("include", includes.join) if includes
+        params.add("sort", sort) if sort
+      end
+    end
 
-    def get_users : Array(Models::User)
-      res = @rest.get "/api/application/users"
+    def get_users(*, page : Int32? = nil, per_page : Int32? = nil,
+                  filter : {String, String}? = nil, include includes : Array(String)? = nil,
+                  sort : String? = nil) : Array(Models::User)
+      res = @rest.get "/api/application/users?" + resolve_query(page, per_page, filter, includes, sort)
       model = Models::FractalList(Models::User).from_json res.body
 
       model.data.map &.attributes
     end
 
-    def get_user(id : Int32) : Models::User
-      res = @rest.get "/api/application/users/#{id}"
+    def get_user(id : Int32, *, page : Int32? = nil, per_page : Int32? = nil,
+                 filter : {String, String}? = nil, include includes : Array(String)? = nil,
+                 sort : String? = nil) : Models::User
+      res = @rest.get "/api/application/users/#{id}?" + resolve_query(page, per_page, filter, includes, sort)
       model = Models::FractalItem(Models::User).from_json res.body
 
       model.attributes
